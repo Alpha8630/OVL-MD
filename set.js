0ecbfec165ea592af8d5af330f6e9f882e228aa2
Alpha8630 <corleonealph@gmail.com>
2025-02-19 15:21:06 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || ".",
+  PREFIXE: process.env.PREFIXE || "!",
   NOM_OWNER: process.env.NOM_OWNER || "Eldji",
   NUMERO_OWNER: process.env.NUMERO_OWNER |224622503739| "",
   MODE: process.env.MODE || "public",
