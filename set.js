4f2b271bdf6f95dea55708a1d3cbbe7667ddde39
Alpha8630 <corleonealph@gmail.com>
2025-02-19 15:11:49 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,16 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  PREFIXE: process.env.PREFIXE || ".",
+  NOM_OWNER: process.env.NOM_OWNER || "Eldji",
+  NUMERO_OWNER: process.env.NUMERO_OWNER |224622503739| "",
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_m4AX0f0L",
   LEVEL_UP: process.env.LEVEL_UP || "non",
   STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
   STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
   DATABASE: process.env.DATABASE || "postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
-  ANTIDELETE: process.env.ANTIDELETE || "non",
+  ANTIDELETE: process.env.ANTIDELETE || "oio",
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
